4877fd00ef24e808b764cc156d73dd38bca07eec
seblful <alesha.chimba@gmail.com>
2023-03-11 13:50:46 +0300

    [EDIT] add func to plot history of training

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-
 import os
 import random
 import matplotlib.pyplot as plt
@@ -289,3 +288,34 @@ def train(model: torch.nn.Module,
 
     # Return the filled results at the end of the epochs
     return results
+
+
+def plot_loss_curves(results):
+    """
+    Plots training curves of a results dictionary.
+    """
+    loss = results["train_loss"]
+    test_loss = results["test_loss"]
+
+    accuracy = results["train_acc"]
+    test_accuracy = results["test_acc"]
+
+    epochs = range(len(results["train_loss"]))
+
+    plt.figure(figsize=(15, 7))
+
+    # Plot loss
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss, label="train_loss")
+    plt.plot(epochs, test_loss, label="test_loss")
+    plt.title("Loss")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+    # Plot accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, accuracy, label="train_accuracy")
+    plt.plot(epochs, test_accuracy, label="test_accuracy")
+    plt.title("Accuracy")
+    plt.xlabel("Epochs")
+    plt.legend()
